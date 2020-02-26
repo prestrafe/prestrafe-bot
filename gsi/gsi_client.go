210b1df4c8487f4bb248451cb140dea861f06652
Jan Gräfen <223234+jangraefen@users.noreply.github.com>
2020-02-26 21:08:09 +0100

    Add client logging

--- gsi/gsi_client.go
+++ gsi/gsi_client.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
+	"log"
 
 	"github.com/go-resty/resty/v2"
 )
@@ -13,15 +14,18 @@ func GetGameState() (*GameState, error) {
 		R().
 		Get("http://localhost:8337/get")
 	if restErr != nil {
+		log.Fatalln(restErr)
 		return nil, restErr
 	}
 
 	if response.StatusCode() != 200 {
+		log.Fatalf("Expected status '%d' but got '%d', with response: %s\n", 200, response.StatusCode(), response.Body())
 		return nil, errors.New(fmt.Sprintf("Expected status '%d' but got '%d', with response: %s", 200, response.StatusCode(), response.Body()))
 	}
 
 	result := new(GameState)
 	if jsonErr := json.Unmarshal(response.Body(), result); jsonErr != nil {
+		log.Fatalln(jsonErr)
 		return nil, jsonErr
 	}
 
