9c433036bbf3b1d54b7694dd11dea99f0418be21
Jan Gräfen <223234+jangraefen@users.noreply.github.com>
2020-02-28 22:35:16 +0100

    Add tests for command handling

---
+++ twitchbot/command_test.go
@@ -0,0 +1,135 @@
+package twitchbot
+
+import (
+	"testing"
+	"time"
+
+	"github.com/gempir/go-twitch-irc"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCommandBuilderDefaults(t *testing.T) {
+	command := NewChatCommandBuilder("name").build()
+
+	if assert.NotNil(t, command) {
+		assert.Equal(t, "name", command.name)
+		assert.Empty(t, command.aliases)
+		assert.Empty(t, command.parameters)
+		assert.False(t, command.subOnly)
+		assert.Equal(t, 5*time.Second, command.coolDown)
+		assert.NotNil(t, command.handler)
+	}
+}
+
+func TestCommandBuilder(t *testing.T) {
+	command := NewChatCommandBuilder("name").
+		WithAlias("alias1", "alias2").
+		WithParameter("param", false, ".*").
+		WithSubOnly(true).
+		WithCoolDown(10 * time.Second).
+		WithHandler(func(parameters map[string]string) (msg string, err error) {
+			return "", nil
+		}).
+		build()
+
+	if assert.NotNil(t, command) {
+		assert.Equal(t, "name", command.name)
+		assert.Equal(t, []string{"alias1", "alias2"}, command.aliases)
+
+		if assert.Len(t, command.parameters, 1) {
+			parameter := command.parameters[0]
+
+			assert.Equal(t, "param", parameter.name)
+			assert.False(t, parameter.required)
+			assert.Equal(t, ".*", parameter.pattern)
+
+			assert.Equal(t, "<param?>", parameter.String())
+			assert.Equal(t, "(?P<param>.*)?", parameter.getPattern())
+		}
+
+		assert.True(t, command.subOnly)
+		assert.Equal(t, 10*time.Second, command.coolDown)
+		assert.NotNil(t, command.handler)
+	}
+}
+
+func TestMatchesPrefix(t *testing.T) {
+	command := NewChatCommandBuilder("name").
+		WithAlias("alias1", "alias2").
+		WithParameter("param", false, ".*").
+		WithSubOnly(true).
+		WithCoolDown(10 * time.Second).
+		WithHandler(func(parameters map[string]string) (msg string, err error) {
+			return "", nil
+		}).
+		build()
+
+	assert.True(t, command.matchesPrefix("!name"))
+	assert.True(t, command.matchesPrefix("!alias1"))
+	assert.True(t, command.matchesPrefix("!alias2"))
+}
+
+func TestCanExecute(t *testing.T) {
+	publicCommand := NewChatCommandBuilder("name").WithSubOnly(false).build()
+	subOnlyCommand := NewChatCommandBuilder("name").WithSubOnly(true).build()
+
+	normalUser := &twitch.User{Badges: map[string]int{}}
+	subUser := &twitch.User{Badges: map[string]int{"subscriber": 1}}
+	modUser := &twitch.User{Badges: map[string]int{"moderator": 1}}
+	broadcasterUser := &twitch.User{Badges: map[string]int{"broadcaster": 1}}
+
+	assert.True(t, publicCommand.canExecute(normalUser))
+	assert.True(t, publicCommand.canExecute(subUser))
+	assert.True(t, publicCommand.canExecute(modUser))
+	assert.True(t, publicCommand.canExecute(broadcasterUser))
+	assert.False(t, subOnlyCommand.canExecute(normalUser))
+	assert.True(t, subOnlyCommand.canExecute(subUser))
+	assert.True(t, subOnlyCommand.canExecute(modUser))
+	assert.True(t, subOnlyCommand.canExecute(broadcasterUser))
+}
+
+func TestIsOnCoolDown(t *testing.T) {
+	command := NewChatCommandBuilder("name").WithCoolDown(1 * time.Second).build()
+
+	assert.False(t, command.isOnCoolDown())
+	command.lastExecution = time.Now()
+	assert.True(t, command.isOnCoolDown())
+	command.lastExecution = time.Now().Add(-command.coolDown)
+	assert.False(t, command.isOnCoolDown())
+}
+
+func TestParseParameters(t *testing.T) {
+	noParamsCommand := NewChatCommandBuilder("name").build()
+	assert.Empty(t, noParamsCommand.parseParameters("!name"))
+	assert.Empty(t, noParamsCommand.parseParameters("!name ignored"))
+
+	paramsCommand := NewChatCommandBuilder("name").WithParameter("param", false, "[0-9]+").build()
+	assert.Empty(t, paramsCommand.parseParameters("!name"))
+	assert.Empty(t, paramsCommand.parseParameters("!name ignored"))
+	assert.NotEmpty(t, paramsCommand.parseParameters("!name 1337"))
+}
+
+func TestStringer(t *testing.T) {
+	command := NewChatCommandBuilder("name").
+		WithAlias("ignored").
+		WithParameter("param1", true, ".*").
+		WithParameter("param2", false, ".*").
+		build()
+	assert.Equal(t, "!name <param1> <param2?>", command.String())
+}
+
+func TestTryHandle(t *testing.T) {
+	command := NewChatCommandBuilder("name").
+		WithParameter("param", true, "[0-9]+").
+		WithSubOnly(true).
+		build()
+	normalUser := &twitch.User{Badges: map[string]int{}}
+	abyss := func(format string, a ...interface{}) {}
+
+	assert.False(t, command.TryHandle(normalUser, &twitch.Message{Text: ""}, abyss))
+	assert.False(t, command.TryHandle(normalUser, &twitch.Message{Text: "!other"}, abyss))
+	assert.False(t, command.TryHandle(normalUser, &twitch.Message{Text: "!other 42"}, abyss))
+
+	assert.True(t, command.TryHandle(normalUser, &twitch.Message{Text: "!name"}, abyss))
+	assert.True(t, command.TryHandle(normalUser, &twitch.Message{Text: "!name 42"}, abyss))
+}
