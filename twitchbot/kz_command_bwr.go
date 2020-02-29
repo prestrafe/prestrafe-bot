1531200fb5ccd7ad2f84680656872f40ad1d1935
Jan Gräfen <223234+jangraefen@users.noreply.github.com>
2020-02-29 03:31:21 +0100

    Add jump stat commands

--- twitchbot/kz_command_bwr.go
+++ twitchbot/kz_command_bwr.go
@@ -3,20 +3,23 @@ package twitchbot
 import (
 	"errors"
 	"fmt"
+	"strconv"
 
 	"prestrafe-bot/globalapi"
 	"prestrafe-bot/gsi"
 )
 
-func NewWRCommand(gsiClient gsi.Client) ChatCommandBuilder {
-	return NewChatCommandBuilder("wr").
-		WithAlias("gr", "gwr", "top").
+func NewBWRCommand(gsiClient gsi.Client) ChatCommandBuilder {
+	return NewChatCommandBuilder("bwr").
+		WithAlias("bgr", "bgwr", "btop").
+		WithParameter("bonus", false, "[0-9]").
 		WithParameter("map", false, "[A-Za-z0-9_]").
-		WithHandler(createWRHandler(gsiClient))
+		WithHandler(createBWRHandler(gsiClient))
 }
 
-func createWRHandler(gsiClient gsi.Client) ChatCommandHandler {
+func createBWRHandler(gsiClient gsi.Client) ChatCommandHandler {
 	return func(ctx CommandContext) (message string, err error) {
+		bonus, hasBonus := ctx.Parameter("bonus")
 		mapName, hasMapName := ctx.Parameter("map")
 
 		gameState, gsiError := gsiClient.GetGameState()
@@ -24,13 +27,21 @@ func createWRHandler(gsiClient gsi.Client) ChatCommandHandler {
 			return "", errors.New("could not retrieve KZ game play")
 		}
 
+		if !hasBonus {
+			bonus = "1"
+		}
 		if !hasMapName {
 			mapName = gameState.Map.Name
 		}
 
-		nub, pro, apiError := globalapi.GetWorldRecord(mapName, gameState.Player.TimerMode(), 0)
+		bonusNumber, _ := strconv.Atoi(bonus)
+		if bonusNumber < 1 {
+			return fmt.Sprintf("'%s' is not a valid bonus number.", bonus), nil
+		}
 
-		message = fmt.Sprintf("Global Records on %s [%s]: ", mapName, gameState.Player.Clan)
+		nub, pro, apiError := globalapi.GetWorldRecord(mapName, gameState.Player.TimerMode(), bonusNumber)
+
+		message = fmt.Sprintf("Global Records on %s Bonus %d [%s]: ", mapName, bonusNumber, gameState.Player.Clan)
 		if nub != nil && apiError == nil {
 			message += fmt.Sprintf("NUB: %s (%d TP) by %s", nub.FormattedTime(), nub.Teleports, nub.PlayerName)
 		} else {
