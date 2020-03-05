0f3f312a5ecce0e81abeb8004a85148fa021c32d
Jan Gräfen <paradox_haze@live.de>
2020-03-05 21:50:26 +0100

    Fix GSI client path

--- gsiclient/gsi_client.go
+++ gsiclient/gsi_client.go
@@ -32,7 +32,7 @@ func (c *client) GetGameState() (*gsi.GameState, error) {
 	response, restErr := resty.New().
 		R().
 		SetHeader("Authorization", fmt.Sprintf("GSI %s", c.authToken)).
-		Get(fmt.Sprintf("http://%s:%d/get", c.host, c.port))
+		Get(fmt.Sprintf("http://%s:%d/", c.host, c.port))
 	if restErr != nil {
 		log.Println(restErr)
 		return nil, restErr
