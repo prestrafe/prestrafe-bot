f22a1fbee5f3e3aa7bbc5c40bb7cb91e985eef12
Jan Gräfen <paradox_haze@live.de>
2020-03-08 14:28:32 +0100

    Add param to JS command

--- twitchbot/kz_command_jumpstat.go
+++ twitchbot/kz_command_jumpstat.go
@@ -11,17 +11,23 @@ import (
 func NewJumpStatCommand(gsiClient gsiclient.Client, apiClient globalapi.Client, name, jumpType, jumpName string, maxDistance int, withoutBinds bool) ChatCommandBuilder {
 	return NewChatCommandBuilder(name).
 		WithAlias(fmt.Sprintf("%spb", name), jumpType).
+		WithParameter("binds", false, "(bind|nobind)").
 		WithHandler(func(ctx CommandContext) (message string, err error) {
+			noBind := withoutBinds
+			if bindsParam, present := ctx.Parameter("noBind"); present {
+				noBind = bindsParam == "nobind"
+			}
+
 			gameState, gsiError := gsiClient.GetGameState()
 			if gsiError != nil || !gsiclient.IsKZGameState(gameState) {
 				return "", errors.New("could not retrieve KZ game play")
 			}
 
-			jumpStat, apiError := (&globalapi.JumpStatServiceClient{Client: apiClient}).GetJumpStatPersonalBest(jumpType, maxDistance, gameState.Provider.SteamId, withoutBinds)
+			jumpStat, apiError := (&globalapi.JumpStatServiceClient{Client: apiClient}).GetJumpStatPersonalBest(jumpType, maxDistance, gameState.Provider.SteamId, noBind)
 			if jumpStat != nil && apiError == nil {
-				binds := "no binds"
+				binds := "no noBind"
 				if jumpStat.HasBinds() {
-					binds = "with binds"
+					binds = "with noBind"
 				}
 
 				return fmt.Sprintf("%s record of %s: %.04f units, %d strafes, %s", jumpName, ctx.Channel(), jumpStat.Distance, jumpStat.StrafeCount, binds), nil
